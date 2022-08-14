a070adb443dab1fad6749b7d6b3ec52122fde70a
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2022-08-13 18:05:43 -0600

    just testing some js stuff

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 
+
+alert("I've been imported");
+
+window.onscroll = function(){
+    var top = window.scrollY || document.documentElement.scrollTop;
+    if(top > 100){
+        console.log("Top is greater than 100");
+    }
+    else{
+        console.log("Top is less than 100");
+    }
+};
