33bd00753f3507d2c2ce2abf7fb837159b34a4bd
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2022-08-18 16:53:12 -0600

    many changes

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
 
 
-alert("I've been imported");
+
 
 window.onscroll = function(){
     var top = window.scrollY || document.documentElement.scrollTop;
     if(top > 100){
+        document.getElementById("left-nav").style.display = "flex";
         console.log("Top is greater than 100");
     }
     else{
+        document.getElementById("left-nav").style.display = "none";
         console.log("Top is less than 100");
     }
 };
