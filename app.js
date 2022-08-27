4da0ce83781186b20f17ea7682f8d5ed95268358
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2022-08-27 15:55:43 -0600

    not looking too bad honestly

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ let sectionBtns = document.querySelectorAll('.section-btn');
 
 for(let i = 0; i < sectionBtns.length; i++)
 {
-    sectionBtns[i].addEventListener('click', myFunction);
+    sectionBtns[i].addEventListener('click', activateSection);
 }
 
-function myFunction(e)
+function activateSection(e)
 {
     let sections = document.querySelectorAll('.section');
     let sectionID = e.target.id;
@@ -24,6 +24,12 @@ function myFunction(e)
     let sectionToActivate = document.getElementById(elementToDisplay);
     console.log(sectionToActivate);
     sectionToActivate.classList.add('active');
+    changeSectionTitle(sectionID);
+}
 
-
+function changeSectionTitle(sectionID)
+{
+    let title = document.getElementById("section-title");
+    console.log(title);
+    title.innerHTML = sectionID; 
 }
