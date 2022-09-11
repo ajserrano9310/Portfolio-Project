ff32851d364f2741ffa425b364f3cc7febf9a614
alejandroserrano <u1214728@utah.edu>
2022-09-11 11:32:26 -0600

    all logic seems to be working now

--- public/js/email-handler.js
+++ public/js/email-handler.js
@@ -1,6 +1,7 @@
 
 let submitBtn = document.getElementById("submit");
 submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
 
     let nameInput = document.getElementById('fullName');
     let emailInput = document.getElementById('email');
@@ -12,6 +13,23 @@ submitBtn.addEventListener('click', (e) => {
         emailInput: emailInput.value, 
         message: message.value, 
     }
+
+    console.log(messagge)
+
+    let xhr = new XMLHttpRequest(); 
+    xhr.open("POST", '/');
+    xhr.setRequestHeader('content-type', 'application/json'); 
+    xhr.onload = function(){
+        console.log(xhr.responseText); 
+        if(xhr.response == 'success'){
+            alert('email sent');
+        }
+        else{
+            alert("something went wrong");
+        }
+    }
+
+    xhr.send(JSON.stringify(messagge))
 
 
     console.log(messagge);
--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 const nodemailer = require("nodemailer");
 
+require('dotenv').config();
+
 const PORT = process.env.PORT || 5000;
+envEmail = process.env.EMAIL;
+pass = process.env.PASS; 
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -11,6 +15,36 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
+
+app.post('/', (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+            user: envEmail, 
+            pass: pass
+        }
+    }); 
+
+    console.log("here is what we are getting");
+    console.log(req.body); 
+    const mailOptions = {
+        from: req.body.email, 
+        to: envEmail, 
+        subject: 'message', 
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error){
+            console.log(error);
+            res.send('error', error); 
+        }
+        else 
+        {
+            res.send('success');
+        }
+    })
+} )
 
 app.listen(PORT, ()=> {
     console.log("server running");
